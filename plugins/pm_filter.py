99601bee97afeb1676e0302531be852762ea68dd
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2022-08-30 21:18:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -786,7 +786,8 @@ async def advantage_spell_chok(msg):
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
     dll= await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
                     reply_markup=InlineKeyboardMarkup(btn))
-    
+    await asyncio.sleep(10)
+    await dll.delete()
     await msg.delete()
 
 
