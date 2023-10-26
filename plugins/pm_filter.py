2631faf967217ff588d2fa8c315e6b8d213adac6
Amalchristo123 <139036853+Amalchristo123@users.noreply.github.com>
2023-10-26 07:38:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -730,9 +730,9 @@ async def auto_filter(client, msg, spoll=False):
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
     else:
         dll=await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-        await asyncio.sleep(3600)
-        fll=await dll.edit_text(f"<b>🗑️ Filter Deleted After 1 Hour ‼️ \n 🔍Search Again !!</b>")
-        await asyncio.sleep(60)
+        await asyncio.sleep(300)
+        fll=await dll.edit_text(f"<b>🗑️ Filter Deleted After 5 mins ‼️ \n 🔍Search Again !!</b>")
+        await asyncio.sleep(300)
         await fll.delete()
         await message.delete()
     if spoll:
