3a876cd48c5c6d3d36716ac775efd7af77ef2fe6
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2024-03-23 12:57:49 +0530

    Create route.py

---
+++ plugins/route.py
@@ -0,0 +1,5 @@
+from aiohttp import web
+routes = web.RouteTableDef()
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("Join Telegram Channal @MvM_Links")
